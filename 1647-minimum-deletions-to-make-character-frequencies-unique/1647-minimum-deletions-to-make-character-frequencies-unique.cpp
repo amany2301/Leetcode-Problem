e396b6b307ac46f0d80f1ad462a803f210325c69
Aman Yadav <76835006+amany2301@users.noreply.github.com>
2022-06-11 17:52:06 +0530

    Time: 65 ms (69.79%), Space: 17.5 MB (22.69%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,25 +3,41 @@ public:
     int minDeletions(string s) {
         vector<int> freq(26,0);
         int i, ans = 0;
+        unordered_set<int>st;
         
         //fill the frequency array
-        for(i=0; i<s.size(); i++){
+        for(i=0; i<s.length(); i++){
             freq[s[i] - 'a']++;
         }
         
         //sort in descending order
-        sort(freq.rbegin(), freq.rend());
+         sort(freq.rbegin(), freq.rend());
+        for(auto it: freq)
+        {
+            cout<<it<<" ";
         
-        //counting duplicate frequencies in ans
-        for(i=1; i<freq.size(); i++){
-            if(freq[i] == 0) break;
-            
-            while(freq[i] >= freq[i-1] && freq[i]){
-                freq[i]--;
-                ans++;
-            }
         }
         
+        //counting duplicate frequencies in ans
+        for(i=0; i<freq.size(); i++)
+        {
+            if(freq[i]==0)
+                    break;
+            if(st.find(freq[i])==st.end())
+            {
+                st.insert(freq[i]);   
+            }
+            else
+            {
+                while(st.find(freq[i])!=st.end() and freq[i])
+                { 
+                    freq[i]--;
+                    ans++;
+                }
+                    st.insert(freq[i]);
+                    
+            }
+        }
         return ans;
     }
 };
